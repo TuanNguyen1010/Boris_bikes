22367e789c33cc63f74b91631742389708450489
Student <student@Makerss-MacBook-Air.local>
2019-11-13 16:12:41 +0000

    Renamed dockingstation file, failed release_bike method and then passed it with simple case

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
-require 'dockingstation'
+require 'docking_station'
 
 describe DockingStation do
+  describe "#release_bike" do
+    # context "release bike" do
+    #   specify { subject.respond_to? (release_bike).should be_true }
+    # end\
+    it { expect(subject).to respond_to(:release_bike) }
 
+  end
 end
