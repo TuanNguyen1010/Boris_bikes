26fdbf4d6588b5969d27fc7d0fd747ae735ab575
Student <student@Makerss-MacBook-Air.local>
2019-11-13 23:59:48 +0000

    Tested raise error when there is no bike avaiable in the docking station. Changed release_bike method in DockingStaion.rb to catch the error.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :bike
+  # def initialize(bike)
+  #   @bike = bike
+  # end
+
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
+
+  def dock(bike)
+    # Use an instance variable to store the bike
+    # in the 'state' of this instance
+    @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,20 @@
 require 'docking_station'
 
 describe DockingStation do
-  describe "#release_bike" do
-    # context "release bike" do
-    #   specify { subject.respond_to? (release_bike).should be_true }
-    # end\
     it { expect(subject).to respond_to(:release_bike) }
-
-    it 'releases working bikes' do
-      bike = subject.release_bike
-      expect(bike).to be_working
+    describe '#release_bike' do
+      it 'raises an error when there are no bikes available' do
+        # we want to release the bike we docked
+        # the reason we use curly braces instead of parentheses can be explained here
+        # https://stackoverflow.com/questions/21567838/when-to-use-curly-braces-vs-parenthesis-in-expect-rspec-method
+        expect{ subject.release_bike }.to raise_error(RuntimeError, 'No bikes available')
+      end
     end
 
-  end
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
 end
