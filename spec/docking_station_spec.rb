f0f179d5a187521a485233b3ea47d4a01d1725d3
Student <student@Makerss-MacBook-Air.local>
2019-11-13 16:54:32 +0000

    Added test to test if bike is working. Tested if the bike released from docking station is working. To pass test made all bikes working? to be true.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,5 +7,10 @@ describe DockingStation do
     # end\
     it { expect(subject).to respond_to(:release_bike) }
 
+    it 'releases working bikes' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
   end
 end
