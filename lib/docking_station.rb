6ad7e06275ef9143039789a53422471009494052
Student <student@Makerss-MacBook-Air.local>
2019-11-14 16:04:46 +0000

    Step 14 added bikes instance varaible

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,20 +2,25 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :bike
+  attr_reader :bikes
   # def initialize(bike)
   #   @bike = bike
   # end
 
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
     # Use an instance variable to store the bike
     # in the 'state' of this instance
-    @bike = bike
+    fail 'No spaces available' if @bikes.count >= 20
+    @bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,20 +1,28 @@
 require 'docking_station'
 
 describe DockingStation do
-    it { expect(subject).to respond_to(:release_bike) }
+    station = DockingStation.new
     describe '#release_bike' do
       it 'raises an error when there are no bikes available' do
         # we want to release the bike we docked
         # the reason we use curly braces instead of parentheses can be explained here
         # https://stackoverflow.com/questions/21567838/when-to-use-curly-braces-vs-parenthesis-in-expect-rspec-method
-        expect{ subject.release_bike }.to raise_error(RuntimeError, 'No bikes available')
+        expect{ station.release_bike }.to raise_error(RuntimeError, 'No bikes available')
       end
     end
 
-    it { is_expected.to respond_to(:dock).with(1).argument }
-
-    it 'docks something' do
-      bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+    describe '#dock' do
+      it 'raises an error when there are no spaces available' do
+        # bike = Bike.new
+        20.times { station.dock Bike.new }
+        expect{ station.dock Bike.new }.to raise_error(RuntimeError, 'No spaces available')
+      end
     end
+
+    # it { is_expected.to respond_to(:dock).with(1).argument }
+    #
+    # it 'docks something' do
+    #   bike = Bike.new
+    #   expect(subject.dock(bike)).to eq bike
+    # end
 end
