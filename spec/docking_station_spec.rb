79583159dbd2b5f26d0f0380eb18b6a834f2dd05
Student <student@Makerss-MacBook-Air.local>
2019-11-16 23:07:32 +0000

    changed initializaton method in DockingStation.rb and made some unit tests for newly implemented method.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,30 +1,41 @@
 require 'docking_station'
 
 describe DockingStation do
-    station = DockingStation.new
-    describe '#release_bike' do
-      it 'raises an error when there are no bikes available' do
-        # we want to release the bike we docked
-        # the reason we use curly braces instead of parentheses can be explained here
-        # https://stackoverflow.com/questions/21567838/when-to-use-curly-braces-vs-parenthesis-in-expect-rspec-method
-        expect{ station.release_bike }.to raise_error(RuntimeError, 'No bikes available')
-      end
-    end
+  it "create docking station with a max capacity of 1" do
+    station1 = DockingStation.new(1)
+    expect( station1.capacity ).to eq(1)
+  end
 
-    describe '#dock' do
-      it 'raises an error when there are no spaces available' do
-        # bike = Bike.new
-        20.times { station.dock Bike.new }
-        expect{ station.dock Bike.new }.to raise_error(RuntimeError, 'No spaces available')
-      end
-    end
+  it 'create docking station with a default max capacity 20' do
+    station20 = DockingStation.new
+    expect( station20.capacity ).to eq(20)
+  end
 
-    it { is_expected.to respond_to(:dock).with(1).argument }
-    it { is_expected.to respond_to(:bikes) }
-    it { is_expected.to respond_to(:release_bike)}
-    #
-    # it 'docks something' do
-    #   bike = Bike.new
-    #   expect(subject.dock(bike)).to eq bike
-    # end
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      station = DockingStation.new
+      # we want to release the bike we docked
+      # the reason we use curly braces instead of parentheses can be explained here
+      # https://stackoverflow.com/questions/21567838/when-to-use-curly-braces-vs-parenthesis-in-expect-rspec-method
+      expect{ station.release_bike }.to raise_error(RuntimeError, 'No bikes available')
+    end
+  end
+
+  describe '#dock' do
+    it 'raises an error when there are no spaces available' do
+      # bike = Bike.new
+      station = DockingStation.new
+      20.times { station.dock Bike.new }
+      expect{ station.dock Bike.new }.to raise_error(RuntimeError, 'No spaces available')
+    end
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bikes) }
+  it { is_expected.to respond_to(:release_bike)}
+  #
+  # it 'docks something' do
+  #   bike = Bike.new
+  #   expect(subject.dock(bike)).to eq bike
+  # end
 end
